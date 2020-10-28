ceec2f924519fc146e761e707705d36ea051f772
Cody Chen <codychen1@gmail.com>
2020-10-28 15:57:21 -0600

    added variables to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,5 @@
 var searchBtn = document.getElementById("search-button");
+var searchInput = document.getElementById("inlineFormInput");
+var savedCities = document.getElementById("saved-cities");
+var currentCity = document.getElementById("current-city");
+var fiveDayForecast = document.getElementById("five-day-forecast");
