2c5b29626378f4df0aeb99651c898dec1ca793e8
Cody Chen <codychen1@gmail.com>
2020-11-01 11:49:53 -0700

    added fetch 5 day and uv request

--- Develop/script.js
+++ Develop/script.js
@@ -4,47 +4,85 @@ var clearCity = document.getElementById("clear-history");
 var searchBtn = document.getElementById("search-button");
 var cityName = document.getElementById("city-name");
 var cityPic = document.getElementById("current-pic");
-var citytemp = document.getElementById("city-temperature");
+var cityTemp = document.getElementById("city-temperature");
 var cityHumidity = document.getElementById("city-humidity");
 var cityWindSpeed = document.getElementById("city-windspeed");
 var cityUV = document.getElementById("city-UV");
 var cityHistory = document.getElementById("history");
 var searchHistory = JSON.parse(localStorage.getItem("search"));
+var APIKey = "&appid=11cc6738fb7101f2239490031655308f&units=imperial";
 
 //FUNCTION DECLARATION
 
-function searchedCity(name) {
+function searchWeather(name) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     name +
-    "&appid=11cc6738fb7101f2239490031655308f&units=imperial";
+    "&appid=" +
+    APIKey;
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.main.temp);
+      console.log(data);
 
-      var nameValue = data.name;
-      var tempValue = data.main.temp;
-      var windValue = data.wind;
-      var uvValue = data["uv"];
+      var currentDate = new Date(data.dt * 1000);
+      console.log(currentDate);
 
-      cityName = nameValue;
-      citytemp.textContent = tempValue;
-      "city-windspeed".innerHTML = windValue;
-      "city-UV".innerHTML = uvValue;
+      var day = currentDate.getDate();
+      var month = currentDate.getMonth() + 1;
+      var year = currentDate.getFullYear();
+
+      cityName.textContent =
+        response.data.name + (month + "/" + day + "/" + year);
+
+      var weatherImage = response.data.weather[0].icon;
+      cityPic.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" + weatherImage + "@2x.png"
+      );
+      cityPic.setAttribute("alt", response.data.weather[0].description);
+      cityTemp.textContent =
+        "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+      cityTemp.textContent = "Humidity: " + response.data.main.humidity + "%";
+      cityWindSpeed.textContent =
+        "Wind Speed: " + response.data.wind.speed + " Miles Per Hour";
+    });
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var latitude = response.data.coord.lat;
+      var longitude = response.data.coord.lon;
+
+      var UVUrl =
+        "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        APIKey +
+        "&cnt=1";
+
+      let UVIndex = document.createElement("span");
+      UVIndex.setAttribute("class", "badge badge-danger");
+      UVIndex.innerHTML = response.data[0].value;
+      currentUVEl.innerHTML = "UV Index: ";
+      currentUVEl.append(UVIndex);
     });
 }
 
-function listCity() {
-  var cityText = $(this).siblings(".text").val();
+// function listCity() {
+//   var cityText = $(this).siblings(".text").val();
 
-  localStorage.setItem(key, value);
-}
+//   localStorage.setItem(key, value);
+// }
 
-//EVENTS
+// //EVENTS
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
   console.log(searchValue.value);
